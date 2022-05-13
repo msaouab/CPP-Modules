14430cc91d308e64e681c4b552956f1887661491
msaouab <saouab.mohammed@gmail.com>
2022-05-13 18:31:21 +0100

    ex06 done

---
+++ CPP_Module_01/ex06/Harl.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msaouab <msaouab@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/13 12:21:19 by msaouab           #+#    #+#             */
+/*   Updated: 2022/05/13 18:29:24 by msaouab          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./Harl.hpp"
+
+Harl::Harl() {
+}
+
+Harl::~Harl() {
+}
+
+void	Harl::debug() {
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese";
+	std::cout << "-triple-pickle-special-ketchup burger. I really do!";
+	std::cout << std::endl << std::endl;
+	info();
+}
+void	Harl::info() {
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more mone";
+	std::cout << ". You didn’t putenough bacon in my burger! If you did, ";
+	std::cout << "I wouldn’t be asking for more!";
+	std::cout << std::endl << std::endl;
+	warning();
+}
+void	Harl::warning() {
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been ";
+	std::cout << "coming foryears whereas you started working here since last month.";
+	std::cout << std::endl << std::endl;
+	error();
+}
+void	Harl::error() {
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now.";
+	std::cout << std::endl << std::endl;
+}
+
+void	Harl::complain(std::string level) {
+	int i;
+	std::string choise[4] = {
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"
+	};
+	for (i = 0; i < 4; i++)
+		if (choise[i] == level)
+			break ;
+	switch (i)
+	{
+		case 0:
+			debug();
+			break;
+		case 1:
+			info();
+			break;
+		case 2:
+			warning();
+			break;
+		case 3:
+			error();
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
+}
