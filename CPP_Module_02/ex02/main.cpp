c6de3fa26a30f73eaf7fc48162c45f2671646206
msaouab <saouab.mohammed@gmail.com>
2022-05-18 14:43:33 +0100

    cpp02 ex02 done

---
+++ CPP_Module_02/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msaouab <msaouab@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/16 17:18:24 by msaouab           #+#    #+#             */
+/*   Updated: 2022/05/18 12:11:41 by msaouab          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./Fixed.hpp"
+
+int	main(void)
+{
+	Fixed		a;
+	Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ) );
+	
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max( a, b ) << std::endl;
+
+	return 0;
+}
