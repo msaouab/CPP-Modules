9a38c5471ce7326933e10e8860de579bd402f530
msaouab <saouab.mohammed@gmail.com>
2022-06-21 21:24:32 +0100

    cpp06

---
+++ CPP_Module_06/ex00/Conversion.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Conversion.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msaouab <msaouab@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 20:47:43 by msaouab           #+#    #+#             */
+/*   Updated: 2022/06/21 20:49:55 by msaouab          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONVERSION_HPP
+#define  CONVERSION_HPP
+
+# include <iostream>
+
+class Conversion
+{
+	private:
+	public:
+		Conversion();
+		~Conversion();
+};
+
+#endif
